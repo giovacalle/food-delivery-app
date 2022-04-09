e78f041d15e13e247cdc8c6141563c4de9d88964
Giovanni <giovacalle.dev@gmail.com>
2022-04-09 17:54:44 +0200

    Update use-input.js

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -5,43 +5,37 @@ const initialInputState = {
     isTouched: false
 };
 const inputStateReducer = (state, action) => {
+    let initialState = initialInputState;
+
     switch (action.type) {
         case 'INPUT':
-            return { ...state, value: action.value };
+            initialState = { ...state, value: action.value };
             break;
         case 'BLUR':
-            return { ...state, isTouched: true };
-            break;
-        case 'RESET':
-            return initialInputState;
+            initialState = { ...state, isTouched: true };
             break;
         default:
-            return initialInputState;
             break;
     }
+
+    return initialState;
 };
 
 const useInput = (validateValue) => {
     const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
-    // const [enteredValue, setEnteredValue] = useState('');
-    // const [isTouched, setIsTouched] = useState(false); --> change with useReducer
 
     const valueIsValid = validateValue(inputState.value);
     const hasError = !valueIsValid && inputState.isTouched;
 
     const valueChangeHandler = (event) => {
         dispatch({ type: 'INPUT', value: event.target.value });
-        // setEnteredValue(event.target.value);
     };
 
     const inputBlurHandler = event => {
-        // setIsTouched(true);
         dispatch({ type: 'BLUR' });
     };
 
     const reset = () => {
-        // setEnteredValue('');
-        // setIsTouched(false);
         dispatch({ type: 'RESET' });
     };
 
